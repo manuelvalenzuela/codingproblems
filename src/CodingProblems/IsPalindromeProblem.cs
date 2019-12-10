ff26718257f5766511433d6cdf277b351f430682
Manuel Valenzuela V <manuel@dreamsit.cl>
2019-12-10 01:04:43 -0300

    IsPalindrome (leetcode 125)

---
+++ src/CodingProblems/IsPalindromeProblem.cs
@@ -0,0 +1,58 @@
+﻿namespace CodingProblems
+{
+    using System.Collections.Generic;
+
+    class IsPalindromeProblem
+    {
+        public bool IsPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return true;
+            }
+
+            var validChars = new HashSet<char>("abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray());
+            var left = 0;
+            var right = s.Length - 1;
+
+            while (left < right)
+            {
+                var notKnowIfIsChar = char.ToLowerInvariant(s[left]);
+                while (!validChars.Contains(notKnowIfIsChar))
+                {
+                    left++;
+                    if (left > right)
+                    {
+                        return true;
+                    }
+
+                    notKnowIfIsChar = char.ToLowerInvariant(s[left]);
+                }
+
+                var leftChar = notKnowIfIsChar;
+
+                notKnowIfIsChar = char.ToLowerInvariant(s[right]);
+                while (!validChars.Contains(notKnowIfIsChar))
+                {
+                    right--;
+                    if (right < 0)
+                    {
+                        return true;
+                    }
+
+                    notKnowIfIsChar = char.ToLowerInvariant(s[right]);
+                }
+
+                var rightChar = notKnowIfIsChar;
+
+                if (leftChar != rightChar)
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
