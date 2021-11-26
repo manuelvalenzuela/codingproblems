dbc61d7c208c343bdcbb5b8446f597fffce969fb
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-11-26 13:11:37 -0800

    ListPartition

---
+++ src/CodingProblems/ListPartition.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+    public class ListPartition
+    {
+        public void Partition(List<int> list, int k)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] < k)
+                {
+                    Swap(list, i, 0);
+                }
+                else if (list[i] > k)
+                {
+                    Swap(list, i, list.Count - 1);
+                }
+            }
+        }
+
+        private void Swap(List<int> list, int i, int j)
+        {
+            int tmp = list[j];
+            list[j] = list[i];
+            list[i] = tmp;
+        }
+    }
+}
---
+++ src/CodingProblemsTests/ListPartitionTests.cs
@@ -0,0 +1,70 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class ListPartitionTests
+    {
+        [Fact]
+        public void Partition_EmptyList()
+        {
+            var listPartition = new ListPartition();
+
+            List<int> list = new() { };
+
+            listPartition.Partition(list, 3);
+
+            list.Should().BeEquivalentTo(new List<int>() { });
+        }
+
+        [Fact]
+        public void Partition_OneElement()
+        {
+            var listPartition = new ListPartition();
+
+            List<int> list = new() { 4 };
+
+            listPartition.Partition(list, 3);
+
+            list.Should().BeEquivalentTo(new List<int>() { 4 });
+        }
+
+        [Fact]
+        public void Partition_BasicCase()
+        {
+            var listPartition = new ListPartition();
+
+            List<int> list = new() { 4, 5, 3, 2, 1, 0 };
+
+            listPartition.Partition(list, 3);
+
+            list.Should().BeEquivalentTo(new List<int>() { 2, 1, 0, 3, 4, 5 });
+        }
+
+        [Fact]
+        public void Partition_Repeated()
+        {
+            var listPartition = new ListPartition();
+
+            List<int> list = new() { -4, 5, 3, 3, 2, 1, 7, -4, 3, 2, 0 };
+
+            listPartition.Partition(list, 3);
+
+            list.Should().BeEquivalentTo(new List<int>() { -4, 2, 1, 2, 0, -4, 3, 3, 3, 5, 7 });
+        }
+
+        [Fact]
+        public void Partition_RepeatedWhithoutK()
+        {
+            var listPartition = new ListPartition();
+
+            List<int> list = new() { -4, 5, 2, 1, 7, -4, 2, 0 };
+
+            listPartition.Partition(list, 3);
+
+            list.Should().BeEquivalentTo(new List<int>() { -4, 2, 1, 2, 0, -4, 5, 7 });
+        }
+    }
+}
