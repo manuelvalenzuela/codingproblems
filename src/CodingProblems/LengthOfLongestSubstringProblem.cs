c5c559db5ab3ec90a2efac14b0f8c6b23519d686
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-11-27 12:16:46 -0800

    LengthOfLongestSubstringProblem

---
+++ src/CodingProblems/LengthOfLongestSubstringProblem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace CodingProblems
+{
+    public class LengthOfLongestSubstringProblem
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int result = 0;
+
+            Queue<char> alreadyPresent = new();
+
+            foreach (char c in s)
+            {
+                while (alreadyPresent.Contains(c))
+                {
+                    alreadyPresent.Dequeue();
+                }
+
+                alreadyPresent.Enqueue(c);
+                
+                if (alreadyPresent.Count > result)
+                {
+                    result = alreadyPresent.Count;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/CodingProblemsTests/LengthOfLongestSubstringProblemTests.cs
@@ -0,0 +1,73 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class LengthOfLongestSubstringProblemTests
+    {
+        [Fact]
+        public void LengthOfLongestSubstring_Example1()
+        {
+            LengthOfLongestSubstringProblem problem = new();
+
+            problem.LengthOfLongestSubstring("abcabcbb").Should().Be(3);
+        }
+
+        [Fact]
+        public void LengthOfLongestSubstring_Example2()
+        {
+            LengthOfLongestSubstringProblem problem = new();
+
+            problem.LengthOfLongestSubstring("bbbbb").Should().Be(1);
+        }
+
+        [Fact]
+        public void LengthOfLongestSubstring_Example3()
+        {
+            LengthOfLongestSubstringProblem problem = new();
+
+            problem.LengthOfLongestSubstring("pwwkew").Should().Be(3);
+        }
+
+        [Fact]
+        public void LengthOfLongestSubstring_Example4()
+        {
+            LengthOfLongestSubstringProblem problem = new();
+
+            problem.LengthOfLongestSubstring("").Should().Be(0);
+        }
+
+        [Fact]
+        public void LengthOfLongestSubstring_Example5()
+        {
+            LengthOfLongestSubstringProblem problem = new();
+
+            problem.LengthOfLongestSubstring(" ").Should().Be(1);
+        }
+
+        [Fact]
+        public void LengthOfLongestSubstring_Example6()
+        {
+            LengthOfLongestSubstringProblem problem = new();
+
+            problem.LengthOfLongestSubstring("au").Should().Be(2);
+        }
+
+        [Fact]
+        public void LengthOfLongestSubstring_Example7()
+        {
+            LengthOfLongestSubstringProblem problem = new();
+
+            problem.LengthOfLongestSubstring("dvdf").Should().Be(3);
+        }
+
+        [Fact]
+        public void LengthOfLongestSubstring_Example8()
+        {
+            LengthOfLongestSubstringProblem problem = new();
+
+            problem.LengthOfLongestSubstring("abcdcefgc").Should().Be(5);
+        }
+    }
+}
