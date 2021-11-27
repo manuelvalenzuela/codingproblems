36c2e3c740fc3c97105dc83c44f928ca0dd4863b
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-11-26 18:41:05 -0800

    TwoSumProblem

--- src/CodingProblems/TwoSumProblem.cs
+++ src/CodingProblems/TwoSumProblem.cs
@@ -4,36 +4,25 @@ namespace CodingProblems
 {
     public class TwoSumProblem
     {
-        public static int[] TwoSum1(int[] nums, int target)
-        {
-            for (var i = 0; i < nums.Length - 1; i++)
-            {
-                var lookingFor = target - nums[i];
-                for (var j = i + 1; j < nums.Length; j++)
-                {
-                    if (nums[j] == lookingFor)
-                    {
-                        return new[] { i, j };
-                    }
-                }
-            }
-            return null;
-        }
-
         public int[] TwoSum(int[] nums, int target)
         {
-            var rests = new Dictionary<int, int>();
 
-            for (var i = 0; i < nums.Length; i++)
+            Dictionary<int, int> alreadyPresents = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
             {
-                if (rests.ContainsKey(target - nums[i]))
+                if (alreadyPresents.ContainsKey(target - nums[i]))
                 {
-                    return new int[] { rests[target - nums[i]], i };
+                    return new int[] { alreadyPresents[target - nums[i]], i };
                 }
-                rests.Add(nums[i], i);
+
+                if (!alreadyPresents.ContainsKey(nums[i]))
+                {
+                    alreadyPresents.Add(nums[i], i);
+                }
             }
 
-            return new int[] { 0, 0 };
+            return new int[] { -1, -1 };
         }
     }
 }
