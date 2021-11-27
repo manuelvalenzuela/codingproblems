e1a334cf9c849dc9e95b908488e39bc1d54f315b
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-11-26 22:14:18 -0800

    Add Two Numbers

---
+++ src/CodingProblems/AddTwoNumbersProblem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+    public class AddTwoNumbersProblem
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode resultNode = new ListNode(0);
+            ListNode pointer = resultNode;
+            int carryOver = 0;
+            while (true)
+            {
+                int num1 = GetNodeValue(ref l1);
+
+                int num2 = GetNodeValue(ref l2);
+
+                int res = (num1 + num2 + carryOver) % 10;
+                carryOver = (num1 + num2 + carryOver) / 10;
+
+                pointer.next = new ListNode(res);
+                pointer = pointer.next;
+                
+                if (l1 == null && l2 == null && carryOver == 0)
+                {
+                    resultNode = resultNode.next;
+                    return resultNode;
+                }
+            }
+        }
+
+        private int GetNodeValue(ref ListNode node)
+        {
+            if (node != null)
+            {
+                int val = node.val;
+                node = node.next;
+                return val;
+            }
+            return 0;
+        }
+    }
+}
---
+++ src/CodingProblemsTests/AddTwoNumbersProblemTests.cs
@@ -0,0 +1,45 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class AddTwoNumbersProblemTests
+    {
+        [Fact]
+        public void AddTwoNumbers_Basic()
+        {
+            AddTwoNumbersProblem.ListNode l1 = new(2, new(4, new(3)));
+            AddTwoNumbersProblem.ListNode l2 = new(5, new(6, new(4)));
+
+            AddTwoNumbersProblem addTwoNumbersProblem = new();
+            var result = addTwoNumbersProblem.AddTwoNumbers(l1, l2);
+
+            result.Should().BeEquivalentTo(new AddTwoNumbersProblem.ListNode(7, new(0, new(8))));
+        }
+
+        [Fact]
+        public void AddTwoNumbers_Basic2()
+        {
+            AddTwoNumbersProblem.ListNode l1 = new(0);
+            AddTwoNumbersProblem.ListNode l2 = new(0);
+
+            AddTwoNumbersProblem addTwoNumbersProblem = new();
+            var result = addTwoNumbersProblem.AddTwoNumbers(l1, l2);
+
+            result.Should().BeEquivalentTo(new AddTwoNumbersProblem.ListNode(0));
+        }
+
+        [Fact]
+        public void AddTwoNumbers_Basic3()
+        {
+            AddTwoNumbersProblem.ListNode l1 = new(9, new(9, new(9, new(9, new(9, new(9,new(9)))))));
+            AddTwoNumbersProblem.ListNode l2 = new(9, new(9, new(9, new(9))));
+
+            AddTwoNumbersProblem addTwoNumbersProblem = new();
+            var result = addTwoNumbersProblem.AddTwoNumbers(l1, l2);
+
+            result.Should().BeEquivalentTo(new AddTwoNumbersProblem.ListNode(8, new(9, new(9, new(9, new(0, new(0, new(0, new(1)))))))));
+        }
+    }
+}
