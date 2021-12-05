bbc8dd876ccfa3cf275b05491a3008c4201f5175
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-12-04 17:47:15 -0800

    Jump Game 2

---
+++ src/CodingProblems/JumpGame2Problem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CodingProblems
+{
+    public class JumpGame2Problem
+    {
+        public int Jump(int[] nums)
+        {
+            int jumps = 0;
+            int max = 0;
+            int last = 0;
+            
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                max = Math.Max(max, nums[i] + i);
+                if (i == last)
+                {
+                    last = max;
+                    jumps++;
+                }
+            }
+
+            return jumps;
+        }
+    }
+}
---
+++ src/CodingProblemsTests/JumpGame2ProblemTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using FluentAssertions;
+using CodingProblems;
+
+namespace CodingProblemsTests
+{
+    public class JumpGame2ProblemTests
+    {
+        [Fact]
+        public void Jump_Example1()
+        {
+            JumpGame2Problem problem = new();
+            problem
+                .Jump(new int[] { 2, 3, 1, 1, 4 })
+                .Should().Be(2);
+        }
+
+        [Fact]
+        public void Jump_Example2()
+        {
+            JumpGame2Problem problem = new();
+            problem
+                .Jump(new int[] { 2, 3, 0, 1, 4 })
+                .Should().Be(2);
+        }
+
+        [Fact]
+        public void Jump_Example3()
+        {
+            JumpGame2Problem problem = new();
+            problem
+                .Jump(new int[] { 2, 1, 2, 3, 2, 2, 0, 9 })
+                .Should().Be(4);
+        }
+    }
+}
