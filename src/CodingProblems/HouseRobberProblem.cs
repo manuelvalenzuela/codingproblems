703b6e69b6f56acbe5f8d8a1b9d9c7cefc08271e
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-12-07 01:29:17 -0800

    House Robber Problem

---
+++ src/CodingProblems/HouseRobberProblem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CodingProblems
+{
+    public class HouseRobberProblem
+    {
+        public int Rob(int[] nums)
+        {
+            int[] memo = new int[nums.Length];
+
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            if (nums.Length >= 2)
+            {
+                memo[0] = nums[0];
+                memo[1] = Math.Max(nums[0], nums[1]);
+            }
+
+            for (int i = 2; i < nums.Length; i++)
+            {
+                memo[i] = Math.Max(nums[i] + memo[i - 2], memo[i - 1]);
+            }
+
+            return memo[memo.Length - 1];
+        }
+    }
+}
---
+++ src/CodingProblemsTests/HouseRobberProblemTests.cs
@@ -0,0 +1,34 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class HouseRobberProblemTests
+    {
+        [Fact]
+        public void Rob_Example1()
+        {
+            HouseRobberProblem problem = new ();
+            problem
+                .Rob(new int[] { 1, 2, 3, 1 })
+                .Should()
+                .Be(4);
+        }
+
+        [Fact]
+        public void Rob_Example2()
+        {
+            HouseRobberProblem problem = new ();
+            problem
+                .Rob(new int[] { 2, 1, 1, 2 })
+                .Should()
+                .Be(4);
+        }
+    }
+}
