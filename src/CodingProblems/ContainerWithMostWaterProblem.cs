cdadd91029b92f2b0ccc2307af0298f5f3ade9f0
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-12-08 22:19:23 -0800

    Container With Most Water Problem

---
+++ src/CodingProblems/ContainerWithMostWaterProblem.cs
@@ -0,0 +1,38 @@
+﻿namespace CodingProblems
+{
+    public class ContainerWithMostWaterProblem
+    {
+        public int MaxArea(int[] height)
+        {
+            int leftWall = 0;
+            int rightWall = height.Length - 1;
+            int maxArea = 0;
+
+            while (leftWall < rightWall)
+            {
+                int leftHeight = height[leftWall];
+                int rightHeight = height[rightWall];
+                int distance = rightWall - leftWall;
+                int minHeight = leftHeight > rightHeight ? rightHeight : leftHeight;
+
+                int candidateArea = minHeight * distance;
+
+                if (candidateArea > maxArea)
+                {
+                    maxArea = candidateArea;
+                }
+
+                if (leftHeight > rightHeight)
+                {
+                    rightWall--;
+                }
+                else
+                {
+                    leftWall++;
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
---
+++ src/CodingProblemsTests/ContainerWithMostWaterProblemTests.cs
@@ -0,0 +1,45 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class ContainerWithMostWaterProblemTests
+    {
+        [Fact]
+        public void MaxArea_Example1()
+        {
+            ContainerWithMostWaterProblem containerWithMostWaterProblem = new();
+            containerWithMostWaterProblem.MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 })
+                .Should()
+                .Be(49);
+        }
+
+        [Fact]
+        public void MaxArea_Example2()
+        {
+            ContainerWithMostWaterProblem containerWithMostWaterProblem = new();
+            containerWithMostWaterProblem.MaxArea(new int[] { 1, 1 })
+                .Should()
+                .Be(1);
+        }
+
+        [Fact]
+        public void MaxArea_Example3()
+        {
+            ContainerWithMostWaterProblem containerWithMostWaterProblem = new();
+            containerWithMostWaterProblem.MaxArea(new int[] { 4, 3, 2, 1, 4 })
+                .Should()
+                .Be(16);
+        }
+
+        [Fact]
+        public void MaxArea_Example4()
+        {
+            ContainerWithMostWaterProblem containerWithMostWaterProblem = new();
+            containerWithMostWaterProblem.MaxArea(new int[] { 1, 2, 1 })
+                .Should()
+                .Be(2);
+        }
+    }
+}
