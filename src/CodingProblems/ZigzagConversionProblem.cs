d16e5a310a392bd809d4dc20e12798806b6e6383
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-12-08 19:27:44 -0800

    Zigzag Conversion Problem

---
+++ src/CodingProblems/ZigzagConversionProblem.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace CodingProblems
+{
+    public class ZigzagConversionProblem
+    {
+        public string Convert(string s, int numRows)
+        {
+            if (numRows == 1 || s.Length < numRows)
+            {
+                return s;
+            }
+
+            List<string> map = new();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                map.Add("");
+            }
+
+            Direction direction = Direction.DOWN;
+
+            int row = 0;
+
+            foreach (char c in s)
+            {
+                map[row] = $"{map[row]}{c}";
+
+                if (direction == Direction.DOWN)
+                {
+                    row++;
+
+                    if (row == numRows)
+                    {
+                        row = numRows - 2;
+                        if (row > 0)
+                        {
+                            direction = Direction.RIGHT;
+                        }
+                        else
+                        {
+                            direction = Direction.DOWN;
+                        }
+                    }
+                }
+                else if (direction == Direction.RIGHT)
+                {
+                    row--;
+
+                    if (row <= 0)
+                    {
+                        row = 0;
+                        direction = Direction.DOWN;
+                    }
+                }
+            }
+
+            return string.Join("", map);
+        }
+
+        public enum Direction
+        {
+            DOWN,
+            RIGHT
+        }
+    }
+}
---
+++ src/CodingProblemsTests/ZigzagConversionProblemTests.cs
@@ -0,0 +1,39 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class ZigzagConversionProblemTests
+    {
+        [Fact]
+        public void Convert_Example1()
+        {
+            ZigzagConversionProblem zigzagConversionProblem = new();
+            zigzagConversionProblem
+                .Convert("PAYPALISHIRING", 3)
+                .Should()
+                .Be("PAHNAPLSIIGYIR");
+        }
+
+        [Fact]
+        public void Convert_Example2()
+        {
+            ZigzagConversionProblem zigzagConversionProblem = new();
+            zigzagConversionProblem
+                .Convert("PAYPALISHIRING", 4)
+                .Should()
+                .Be("PINALSIGYAHRPI");
+        }
+
+        [Fact]
+        public void Convert_Example3()
+        {
+            ZigzagConversionProblem zigzagConversionProblem = new();
+            zigzagConversionProblem
+                .Convert("ABCD", 2)
+                .Should()
+                .Be("ACBD");
+        }
+    }
+}
