8640a83e647f3a31df6de14c92fdee77ca135003
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-12-10 03:13:12 -0800

    Symmetric Tree Problem

---
+++ src/CodingProblems/SymmetricTreeProblem.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+    public class SymmetricTreeProblem
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public bool IsSymmetric(TreeNode root)
+        {
+            return IsSymmetricIteratively(root.left, root.right);
+            //return IsSymmetricRecursive(root.left, root.right);
+        }
+
+        private bool IsSymmetricIteratively(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null) return true;
+            if (left == null || right == null) return false; 
+            if (left.val != right.val) return false;
+            
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            stack.Push(left.left);
+            stack.Push(right.right);
+            stack.Push(left.right);
+            stack.Push(right.left);
+
+            while(stack.Any())
+            {
+                TreeNode nodeLeft = stack.Pop();
+                TreeNode nodeRight = stack.Pop();
+
+                if (nodeLeft != null && nodeRight != null)
+                {
+                    if (nodeLeft.val != nodeRight.val) return false;
+                
+                    stack.Push(nodeLeft.left);
+                    stack.Push(nodeRight.right);
+                    stack.Push(nodeLeft.right);
+                    stack.Push(nodeRight.left);
+                }
+                else if (nodeLeft != null || nodeRight != null)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsSymmetricRecursive(TreeNode left, TreeNode right)
+        {
+            if (left == null && right == null) return true;
+            if (left == null || right == null) return false;
+            if (left.val != right.val) return false;
+
+            return IsSymmetricRecursive(left.left, right.right) && IsSymmetricRecursive(left.right, right.left);
+        }
+    }
+}
---
+++ src/CodingProblemsTests/SymmetricTreeProblemTests.cs
@@ -0,0 +1,28 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using Xunit;
+using static CodingProblems.SymmetricTreeProblem;
+
+namespace CodingProblemsTests
+{
+    public class SymmetricTreeProblemTests
+    {
+        [Fact]
+        public void IsSymmetric_Example1()
+        {
+            SymmetricTreeProblem symmetricTreeProblem = new();
+            symmetricTreeProblem.IsSymmetric(new TreeNode(1, new TreeNode(2, new TreeNode(3), new TreeNode(4)), new TreeNode(2, new TreeNode(4), new TreeNode(3))))
+                .Should()
+                .BeTrue();
+        }
+
+        [Fact]
+        public void IsSymmetric_Example2()
+        {
+            SymmetricTreeProblem symmetricTreeProblem = new();
+            symmetricTreeProblem.IsSymmetric(new TreeNode(2, new TreeNode(3, new TreeNode(4), new TreeNode(5)), new TreeNode(3, new TreeNode(5), null)))
+                .Should()
+                .BeFalse();
+        }
+    }
+}
