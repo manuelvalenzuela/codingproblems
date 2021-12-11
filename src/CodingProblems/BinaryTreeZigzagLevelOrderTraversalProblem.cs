dd1473de9f2c84d94afa7dd3c774e46be75c97d3
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-12-10 22:21:51 -0800

    Binary Tree Zigzag Level Order Traversal Problem

---
+++ src/CodingProblems/BinaryTreeZigzagLevelOrderTraversalProblem.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections.Generic;
+
+namespace CodingProblems
+{
+    public class BinaryTreeZigzagLevelOrderTraversalProblem
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            List<IList<int>> result = new();
+            if (root is null) return result;
+
+            Stack<TreeNode> s1 = new();
+            Stack<TreeNode> s2 = new();
+
+            s1.Push(root);
+
+            while (s1.Count > 0 || s2.Count > 0)
+            {
+                List<int> levelN = new();
+
+                while (s1.Count > 0)
+                {
+                    TreeNode node = s1.Pop();
+                    levelN.Add(node.val);
+                    if (node.left is not null)
+                    {
+                        s2.Push(node.left);
+                    }
+                    if (node.right is not null)
+                    {
+                        s2.Push(node.right);
+                    }
+                }
+                result.Add(levelN);
+                levelN = new();
+
+                while (s2.Count > 0)
+                {
+                    TreeNode node = s2.Pop();
+                    levelN.Add(node.val);
+                    if (node.right is not null)
+                    {
+                        s1.Push(node.right);
+                    }
+                    if (node.left is not null)
+                    {
+                        s1.Push(node.left);
+                    }
+                }
+                if (levelN.Count > 0)
+                {
+                    result.Add(levelN);
+                }
+            }
+
+            return result;
+        }
+
+        public enum Movement
+        {
+            Left,
+            Right
+        }
+
+        public IList<IList<int>> ZigzagLevelOrderV2(TreeNode root)
+        {
+            List<IList<int>> result = new();
+            if (root is null) return result;
+
+            Queue<TreeNode> queue = new();
+
+            queue.Enqueue(root);
+
+            Movement movement = Movement.Right;
+
+            while (queue.Count > 0)
+            {
+                List<int> levelN = new();
+                int actualQueueSize = queue.Count;
+
+                for (int i = 0; i < actualQueueSize; i++)
+                {
+                    TreeNode node = queue.Dequeue();
+                    if (movement == Movement.Right)
+                    {
+                        levelN.Add(node.val);
+                    }
+                    else
+                    {
+                        levelN.Insert(0, node.val);
+                    }
+
+                    if (node.left is not null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right is not null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+
+                result.Add(levelN);
+                if (movement == Movement.Right)
+                {
+                    movement = Movement.Left;
+                }
+                else
+                {
+                    movement = Movement.Right;
+                }
+            }
+
+            return result;
+        }
+
+        public IList<IList<int>> ZigzagLevelOrderV3(TreeNode root)
+        {
+            List<IList<int>> result = new();
+            if (root is null) return result;
+
+            Queue<TreeNode> queue = new();
+
+            queue.Enqueue(root);
+
+            Movement movement = Movement.Right;
+
+            while (queue.Count > 0)
+            {
+                List<int> levelN = new();
+                int actualQueueSize = queue.Count;
+
+                for (int i = 0; i < actualQueueSize; i++)
+                {
+                    TreeNode node = queue.Dequeue();
+                    levelN.Add(node.val);
+
+                    if (node.left is not null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right is not null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+
+                if (movement == Movement.Right)
+                {
+                    movement = Movement.Left;
+                }
+                else
+                {
+                    levelN.Reverse();
+                    movement = Movement.Right;
+                }
+                result.Add(levelN);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/CodingProblemsTests/BinaryTreeZigzagLevelOrderTraversalProblemTests.cs
@@ -0,0 +1,24 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class BinaryTreeZigzagLevelOrderTraversalProblemTests
+    {
+        [Fact]
+        public void ZigzagLevelOrder_Example1()
+        {
+            BinaryTreeZigzagLevelOrderTraversalProblem binaryTreeZigzagLevelOrderTraversalProblem = new();
+            var result = binaryTreeZigzagLevelOrderTraversalProblem.ZigzagLevelOrder(new(1, new(2, new(4), new(5)), new(3, new(6), new(7))));
+
+            result[0][0].Should().Be(1);
+            result[1][0].Should().Be(3);
+            result[1][1].Should().Be(2);
+            result[2][0].Should().Be(4);
+            result[2][1].Should().Be(5);
+            result[2][2].Should().Be(6);
+            result[2][3].Should().Be(7);
+        }
+    }
+}
