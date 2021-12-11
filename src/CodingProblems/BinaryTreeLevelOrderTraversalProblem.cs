1d16315974d8e7a3260289650da823509c3032ec
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-12-10 17:15:51 -0800

    Binary Tree Level Order Traversal Problem

---
+++ src/CodingProblems/BinaryTreeLevelOrderTraversalProblem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace CodingProblems
+{
+    public class BinaryTreeLevelOrderTraversalProblem
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            List<IList<int>> result = new ();
+            if (root is null) return result;
+
+            Queue<TreeNode> queue = new();
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                List<int> levelN = new();
+                int actualQueueSize = queue.Count;
+
+                for (int i = 0; i < actualQueueSize; i++)
+                {
+                    TreeNode node = queue.Dequeue();
+                    levelN.Add(node.val);
+                    if (node.left is not null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right is not null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+
+                result.Add(levelN);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/CodingProblemsTests/BinaryTreeLevelOrderTraversalProblemTests.cs
@@ -0,0 +1,25 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+using static CodingProblems.BinaryTreeLevelOrderTraversalProblem;
+
+namespace CodingProblemsTests
+{
+    public class BinaryTreeLevelOrderTraversalProblemTests
+    {
+        [Fact]
+        public void LevelOrder_Example1()
+        {
+            BinaryTreeLevelOrderTraversalProblem binaryTreeLevelOrderTraversalProblem = new ();
+            binaryTreeLevelOrderTraversalProblem.LevelOrder(new(3, new(9), new(20,new(15),new(7))))
+                .Should()
+                .BeEquivalentTo(new List<List<int>>
+                {
+                    new List<int> { 3 },
+                    new List<int> { 9, 20 },
+                    new List<int> { 15, 7 }
+                });
+        }
+    }
+}
