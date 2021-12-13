d821b5f310d5af38c494af72e4b94708e4f4d803
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-12-13 15:19:51 -0800

    Removing Islands Problem

---
+++ src/CodingProblems/RemovingIslandsProblem.cs
@@ -0,0 +1,95 @@
+﻿namespace CodingProblems
+{
+    public class RemovingIslandsProblem
+    {
+        const int LAND = 1;
+        const int SEA = 0;
+        const int NEVER_VISITED = -1;
+        const int WAITING = -2;
+
+        public int[][] RemoveIslands(int[][] matrix)
+        {
+            if (matrix == null)
+            {
+                return null;
+            }
+
+            int height = matrix.Length;
+            int width = matrix[0].Length;
+
+            int[][] visited = new int[height][];
+            for (int row = 0; row < height; row++)
+            {
+                visited[row] = new int[width];
+                for (int col = 0; col < width; col++)
+                {
+                    visited[row][col] = NEVER_VISITED;
+                }
+            }
+
+            for (int row = 1; row < height - 1; row++)
+            {
+                for (int col = 1; col < width - 1; col++)
+                {
+                    if (matrix[row][col] == 1)
+                    {
+                        if (!IsConnectedToEdge(matrix, row, col, visited))
+                        {
+                            matrix[row][col] = SEA;
+                        }
+                    }
+                }
+            }
+            return matrix;
+        }
+
+        private bool IsConnectedToEdge(int[][] matrix, int row, int col, int[][] visited)
+        {
+            if (row < 0 || row >= matrix.Length ||
+                col < 0 || col >= matrix[0].Length)
+            {
+                return true;
+            }
+
+            if (visited[row][col] == WAITING)
+            {
+                return false;
+            }
+
+            if (visited[row][col] == SEA)
+            {
+                return false;
+            }
+
+            if (visited[row][col] == LAND)
+            {
+                return true;
+            }
+
+            if (matrix[row][col] == SEA)
+            {
+                visited[row][col] = SEA;
+                return false;
+            }
+
+            visited[row][col] = WAITING;
+
+            bool isConnectedToEdge =
+                IsConnectedToEdge(matrix, row + 1, col, visited) ||
+                IsConnectedToEdge(matrix, row - 1, col, visited) ||
+                IsConnectedToEdge(matrix, row, col + 1, visited) ||
+                IsConnectedToEdge(matrix, row, col - 1, visited);
+
+            if (isConnectedToEdge)
+            {
+                visited[row][col] = 1;
+            }
+            else
+            {
+                visited[row][col] = 0;
+            }
+
+            return isConnectedToEdge;
+        }
+    }
+}
---
+++ src/CodingProblemsTests/RemovingIslandsProblemTests.cs
@@ -0,0 +1,105 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class RemovingIslandsProblemTests
+    {
+        [Fact]
+        public void RemoveIslands_Example1()
+        {
+            RemovingIslandsProblem removingIslandsProblem = new();
+            removingIslandsProblem.RemoveIslands(null)
+                .Should()
+                .BeNull();
+        }
+
+        [Fact]
+        public void RemoveIslands_Example2()
+        {
+            RemovingIslandsProblem removingIslandsProblem = new();
+
+            int[][] matrix = new int[][]
+            {
+                new int[] { 1, 0, 1, 0, 0, 1 },
+                new int[] { 1, 0, 0, 0, 1, 1 },
+                new int[] { 1, 0, 1, 0, 0, 1 },
+                new int[] { 0, 0, 1, 0, 0, 1 },
+                new int[] { 0, 0, 0, 1, 0, 1 },
+                new int[] { 1, 1, 1, 0, 0, 1 }
+            };
+
+            int[][] expectedResult = new int[][]
+            {
+                new int[] { 1, 0, 1, 0, 0, 1 },
+                new int[] { 1, 0, 0, 0, 1, 1 },
+                new int[] { 1, 0, 0, 0, 0, 1 },
+                new int[] { 0, 0, 0, 0, 0, 1 },
+                new int[] { 0, 0, 0, 0, 0, 1 },
+                new int[] { 1, 1, 1, 0, 0, 1 }
+            };
+
+            int[][] result = removingIslandsProblem.RemoveIslands(matrix);
+            result.Should().BeEquivalentTo(expectedResult, options => options.WithStrictOrdering());
+        }
+
+        [Fact]
+        public void RemoveIslands_Example3()
+        {
+            RemovingIslandsProblem removingIslandsProblem = new();
+
+            int[][] matrix = new int[][]
+            {
+                new int[] { 1, 0, 1, 0, 0, 1 },
+                new int[] { 0, 0, 0, 0, 1, 1 },
+                new int[] { 0, 0, 1, 0, 0, 1 },
+                new int[] { 0, 1, 1, 0, 0, 1 },
+                new int[] { 0, 0, 0, 1, 0, 1 },
+                new int[] { 1, 1, 1, 0, 0, 1 }
+            };
+
+            int[][] expectedResult = new int[][]
+            {
+                new int[] { 1, 0, 1, 0, 0, 1 },
+                new int[] { 0, 0, 0, 0, 1, 1 },
+                new int[] { 0, 0, 0, 0, 0, 1 },
+                new int[] { 0, 0, 0, 0, 0, 1 },
+                new int[] { 0, 0, 0, 0, 0, 1 },
+                new int[] { 1, 1, 1, 0, 0, 1 }
+            };
+
+            int[][] result = removingIslandsProblem.RemoveIslands(matrix);
+            result.Should().BeEquivalentTo(expectedResult, options => options.WithStrictOrdering());
+        }
+
+        [Fact]
+        public void RemoveIslands_Example4()
+        {
+            RemovingIslandsProblem removingIslandsProblem = new();
+
+            int[][] matrix = new int[][]
+            {
+                new int[] { 1, 0, 1, 0, 0, 1 },
+                new int[] { 0, 0, 0, 0, 1, 1 },
+                new int[] { 0, 0, 1, 0, 0, 1 },
+                new int[] { 1, 1, 1, 0, 0, 1 },
+                new int[] { 0, 0, 0, 1, 0, 1 },
+                new int[] { 1, 1, 1, 0, 0, 1 }
+            };
+
+            int[][] expectedResult = new int[][]
+            {
+                new int[] { 1, 0, 1, 0, 0, 1 },
+                new int[] { 0, 0, 0, 0, 1, 1 },
+                new int[] { 0, 0, 1, 0, 0, 1 },
+                new int[] { 1, 1, 1, 0, 0, 1 },
+                new int[] { 0, 0, 0, 0, 0, 1 },
+                new int[] { 1, 1, 1, 0, 0, 1 }
+            };
+
+            int[][] result = removingIslandsProblem.RemoveIslands(matrix);
+            result.Should().BeEquivalentTo(expectedResult, options => options.WithStrictOrdering());
+        }
+    }
+}
