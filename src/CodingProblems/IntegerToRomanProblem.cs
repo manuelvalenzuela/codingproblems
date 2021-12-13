151b6c62600e8a97dabb7ca4969ca2377292b548
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-12-13 11:12:30 -0800

    Integer to Roman Problem

---
+++ src/CodingProblems/IntegerToRomanProblem.cs
@@ -0,0 +1,60 @@
+﻿namespace CodingProblems
+{
+    public class IntegerToRomanProblem
+    {
+        public string IntToRoman(int num)
+        {
+            int m = num / 1000;
+            int c = (num - (m * 1000)) / 100;
+            int x = (num - (m * 1000) - (c * 100)) / 10;
+            int i = num - (m * 1000) - (c * 100) - (x * 10);
+
+            string romanNumber = "";
+
+            for (int im = 1; im <= m; im++)
+            {
+                romanNumber = $"{romanNumber}M";
+            }
+
+            romanNumber = $"{romanNumber}{FillWithRomanNumber(c, 'M', 'D', 'C')}";
+            romanNumber = $"{romanNumber}{FillWithRomanNumber(x, 'C', 'L', 'X')}";
+            romanNumber = $"{romanNumber}{FillWithRomanNumber(i, 'X', 'V', 'I')}";
+
+            return romanNumber;
+        }
+
+        private string FillWithRomanNumber(int number, char tenUnits, char fiveUnits, char unit)
+        {
+            string romanNumber = "";
+
+            if (number > 0)
+            {
+                if (number == 9)
+                {
+                    romanNumber = $"{romanNumber}{unit}{tenUnits}";
+                }
+                else if (number >= 5)
+                {
+                    romanNumber = $"{romanNumber}{fiveUnits}";
+                    for (int i = 6; i <= number; i++)
+                    {
+                        romanNumber = $"{romanNumber}{unit}";
+                    }
+                }
+                else if (number == 4)
+                {
+                    romanNumber = $"{romanNumber}{unit}{fiveUnits}";
+                }
+                else
+                {
+                    for (int i = 1; i <= number; i++)
+                    {
+                        romanNumber = $"{romanNumber}{unit}";
+                    }
+                }
+            }
+
+            return romanNumber;
+        }
+    }
+}
---
+++ src/CodingProblemsTests/IntegerToRomanProblemTests.cs
@@ -0,0 +1,63 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class IntegerToRomanProblemTests
+    {
+        [Fact]
+        public void IntToRoman_Example1()
+        {
+            IntegerToRomanProblem integerToRomanProblem = new();
+            integerToRomanProblem.IntToRoman(3)
+                .Should()
+                .Be("III");
+        }
+
+        [Fact]
+        public void IntToRoman_Example2()
+        {
+            IntegerToRomanProblem integerToRomanProblem = new();
+            integerToRomanProblem.IntToRoman(4)
+                .Should()
+                .Be("IV");
+        }
+        
+        [Fact]
+        public void IntToRoman_Example3()
+        {
+            IntegerToRomanProblem integerToRomanProblem = new();
+            integerToRomanProblem.IntToRoman(9)
+                .Should()
+                .Be("IX");
+        }
+        
+        [Fact]
+        public void IntToRoman_Example4()
+        {
+            IntegerToRomanProblem integerToRomanProblem = new();
+            integerToRomanProblem.IntToRoman(58)
+                .Should()
+                .Be("LVIII");
+        }
+        
+        [Fact]
+        public void IntToRoman_Example5()
+        {
+            IntegerToRomanProblem integerToRomanProblem = new();
+            integerToRomanProblem.IntToRoman(1994)
+                .Should()
+                .Be("MCMXCIV");
+        }
+
+        [Fact]
+        public void IntToRoman_Example6()
+        {
+            IntegerToRomanProblem integerToRomanProblem = new();
+            integerToRomanProblem.IntToRoman(1981)
+                .Should()
+                .Be("MCMLXXXI");
+        }
+    }
+}
