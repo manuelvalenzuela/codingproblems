7915c3ae17efd7b642e1eaebc2cbf21ec251a314
Manuel Valenzuela <manolovalenzuelavega@gmail.com>
2021-12-13 00:48:00 -0800

    Generate Parentheses Problem

---
+++ src/CodingProblems/GenerateParenthesesProblem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace CodingProblems
+{
+    public class GenerateParenthesesProblem
+    {
+        List<string> combinations;
+        public IList<string> GenerateParenthesis(int n)
+        {
+            combinations = new List<string>();
+            Backtrack("", n, n);
+            return combinations;
+        }
+
+        private void Backtrack(string combination, int opened, int closed)
+        {
+            if (opened > closed)
+            {
+                return;
+            }
+            if (opened == 0 && closed == 0)
+            {
+                combinations.Add(combination);
+            }
+            if (opened > 0)
+            {
+                Backtrack($"{combination}(", opened - 1, closed);
+            }
+            if (closed > 0)
+            {
+                Backtrack($"{combination})", opened, closed - 1);
+            }
+        }
+    }
+}
---
+++ src/CodingProblemsTests/GenerateParenthesesProblemTests.cs
@@ -0,0 +1,19 @@
+﻿using CodingProblems;
+using FluentAssertions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CodingProblemsTests
+{
+    public class GenerateParenthesesProblemTests
+    {
+        [Fact]
+        public void GenerateParenthesis_Example1()
+        {
+            GenerateParenthesesProblem generateParenthesesProblem = new();
+            generateParenthesesProblem.GenerateParenthesis(3)
+                .Should()
+                .BeEquivalentTo(new List<string> { "((()))", "(()())", "(())()", "()(())", "()()()" });
+        }
+    }
+}
